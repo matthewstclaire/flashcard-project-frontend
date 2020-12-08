c138a47f34a703956f3256bdfa5a0d3704f3ce5a
matthewxvoorhees <matthew.crnich@gmail.com>
2020-12-08 13:12:34 -0700

    Adding JS

---
+++ fancy.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+    loadFormListener()
+})
+
+function loadFormListener(){
+const postForm = document.getElementById("Answer");
+postForm.addEventListener("submit", function(){
+debugger
+})
+
+}
